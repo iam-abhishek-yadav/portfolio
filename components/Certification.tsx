2a49cd7d05816c1ba49ef6c3b9a1c0d23260065b
iam-abhishek-yadav <abhrecscs17@gmail.com>
2025-02-06 08:33:18 +0530

    add new certification

--- components/Certification.tsx
+++ components/Certification.tsx
@@ -28,6 +28,12 @@ const certifications = [
 		date: "2024",
 		link: "https://www.cloudskillsboost.google/public_profiles/cea11df5-77b1-4b29-96e4-f5de6118b414/badges/7881783",
 	},
+	{
+		name: "Promot Design in Vertex AI",
+		issuer: "Google Cloud",
+		date: "2025",
+		link: "https://www.cloudskillsboost.google/public_profiles/4c0006cb-facb-4d8a-a8db-7d8740cd6338/badges/13837222",
+	},
 ]
 
 export default function Certifications() {
